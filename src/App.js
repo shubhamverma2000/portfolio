1dbf561ba9b8c644fb265dd8c2adf7611edc3143
shubhamverma2000 <[vermashubham0608@gmail.com]>
2023-10-05 17:52:55 +0530

    Navbar and App.js modified using the styled Components- 5thoct

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Hero } from "./components/HeroSection";
+import{ Skills} from "./components/Skills";
+import { Education } from "./components/Education/index";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper =styled.div`
+  background: linear-gradient(
+    38.73deg, 
+    rgba(204, 0, 187, 0.15) 0%, 
+    rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg, 
+      rgba(0, 70, 209, 0) 50%, 
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router >
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
